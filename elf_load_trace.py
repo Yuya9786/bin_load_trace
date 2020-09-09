250f2e23506eb747e69da33f8b2e65b6f18cad1c
vagrant <vagrant@vagrant.vm>
2020-09-09 05:59:41 +0000

    add output

--- elf_load_trace.py
+++ elf_load_trace.py
@@ -21,19 +21,16 @@ int kprobe__elf_map(struct pt_regs *ctx) {
 
 int kretprobe__elf_map(struct pt_regs *ctx) {
     u64 id = bpf_get_current_pid_tgid();
-    char *addr = (void *)PT_REGS_RC(ctx);
+    void *addr = (void *)PT_REGS_RC(ctx);
 
     bpf_trace_printk("ret mmap():\\n");
     bpf_trace_printk("  + pid: %d\\n", id);
-    bpf_trace_printk("  + addr: %x\\n", addr[100]);
+    bpf_trace_printk("  + addr: %x\\n", addr);
+    bpf_trace_printk("  + content: %x\\n", *((char *)addr));
     return 0;
 }
 
 """
 
 bpf = BPF(text = bpf_code)
-#sysmmap_name = bpf.get_syscall_fnname("mmap")
-#bpf.attach_kprobe(event = sysmmap_name, fn_name = "kprobe_mmap")
-#bpf.attach_kretprobe(event = sysmmap_name, fn_name = "kretprobe_mmap")
-#bpf.attach_kprobe(event = sysopenat_name, fn_name = "kprobe_openat")
 bpf.trace_print()
